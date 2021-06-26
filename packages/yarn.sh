86f26e25ed08506d93ac94f728b6dce0ed78e29b
ittp <61617660+ittp@users.noreply.github.com>
2021-06-26 07:04:07 +0300

    Update yarn.sh

--- packages/yarn.sh
+++ packages/yarn.sh
@@ -1,3 +1,4 @@
-  curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-     echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-     sudo apt-get update && sudo apt-get install yarn
+#!/bin/sh
+
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list 
+apt-get update && apt-get install yarn
