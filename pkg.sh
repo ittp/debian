d3be543d835530c8f10628f34b7f47d4a3509b33
ittp <61617660+ittp@users.noreply.github.com>
2021-09-05 00:13:53 +0300

    Create pkg.sh

---
+++ pkg.sh
@@ -0,0 +1,7 @@
+for *.sh in $(ls ./packages)
+do
+  echo $iface
+done
+
+
+SSHTUN_SERVICE="https://gist.githubusercontent.com/ittp/c93098e54d4ea6ba0d9925b742e121b2/raw/365e3e3636a2086566a4e5c76bdee5a1d01e705b/sshtun.sh"
