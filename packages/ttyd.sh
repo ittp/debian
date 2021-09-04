ac1ebf96cdb2b84091fc1f991c561cf4047af6ee
ittp <61617660+ittp@users.noreply.github.com>
2021-09-04 19:33:58 +0300

    Update and rename ttyd to ttyd.sh

--- packages/ttyd.sh
+++ packages/ttyd.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+START=$0
+if [ -e START ]; then
+echo $START
+else 
+echo "no start"
+fi
+
 apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
 git clone https://github.com/tsl0922/ttyd.git
 cd ttyd && mkdir build && cd build
