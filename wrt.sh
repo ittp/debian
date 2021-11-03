29efbc705497d945851c2d3457c4967f2db28207
ittp <61617660+ittp@users.noreply.github.com>
2021-11-03 23:27:35 +0300

    Create wrt.sh

---
+++ wrt.sh
@@ -0,0 +1,31 @@
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+uclient-fetch -O
+ hotplug-extras.sh 
+"https://openwrt.org/_export/code/docs/guide-user/advanced/hotplug_extras?codeblock=0"
+
+
+
+
+
+
+
+
+
+
+
+
+
