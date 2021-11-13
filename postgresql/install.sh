ed940a6515cc2079aedac8737141adf85821af62
ittp <61617660+ittp@users.noreply.github.com>
2021-11-13 04:02:39 +0300

    Update install.sh

--- postgresql/install.sh
+++ postgresql/install.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#!/bin/env sh
 
-deb_install() {
+apt-get update && apt-get install curl sudo -y
 
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get -y install postgresql
+
+sudo_setup() {
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+  sudo apt-get update
+  sudo apt-get -y install postgresql
 }
-# if [ -e ]
-# apt-get update && apt-get install curl sudo -y
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install postgresql
+setup() {
+  echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  apt-get update && apt-get -y install postgresql
+}
+
+echo "Setup PGSQL"
+setup
