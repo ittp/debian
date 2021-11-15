422866ca26ce25b2a97a749a12f3d0b7ddefb8fe
ittp <61617660+ittp@users.noreply.github.com>
2021-11-15 04:10:59 +0300

    Update install.sh

--- postgresql/install.sh
+++ postgresql/install.sh
@@ -6,6 +6,7 @@ apt-get update && apt-get install curl sudo -y
 # wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 # sudo apt-get update
 # sudo apt-get -y install postgresql
+# Create the file repository configuration:
 
 sudo_setup() {
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
